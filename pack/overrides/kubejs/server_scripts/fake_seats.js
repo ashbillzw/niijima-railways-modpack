b601c27753f658155e9ba63b451a857d6d9fd8e1
AshBill <me@ashbill.com>
2025-10-15 18:29:37 -0400

    :wrench: 同步KJS脚本

---
+++ pack/overrides/kubejs/server_scripts/fake_seats.js
@@ -0,0 +1,11 @@
+ServerEvents.tags('block', event => {
+    event.add('create:fake_seats', [
+        'yuushya:couch_raw_crimson_wood'
+    ]);
+    event.add('create:movable_empty_collider', [
+        'yuushya:beef',
+        'yuushya:gas_stove',
+        'yuushya:detergent',
+        'yuushya:jack'
+    ]);
+});
