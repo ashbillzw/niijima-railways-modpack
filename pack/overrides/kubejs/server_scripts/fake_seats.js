41af6853301d60e925b10b62ad29de880e066534
AshBill <me@ashbill.com>
2025-10-26 15:58:37 -0400

    🔧 同步KJS脚本（加入马桶能坐）

--- pack/overrides/kubejs/server_scripts/fake_seats.js
+++ pack/overrides/kubejs/server_scripts/fake_seats.js
@@ -1,11 +1,14 @@
 ServerEvents.tags('block', event => {
-    event.add('create:fake_seats', [
-        'yuushya:couch_raw_crimson_wood'
+    event.add('trainresync:fake_seats', [
+        'yuushya:couch_raw_crimson_wood',
+        'yuushua:toilet',
+        'yuushya:park_bench_raw_oak_wood'
     ]);
     event.add('create:movable_empty_collider', [
         'yuushya:beef',
-        'yuushya:gas_stove',
         'yuushya:detergent',
-        'yuushya:jack'
+        'yuushya:gas_stove',
+        'yuushya:jack',
+        'yuushya:park_bench_raw_oak_wood'
     ]);
 });
