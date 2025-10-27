7bac1422932c968b7ea6bb6b3e6357509cdfd10e
AshBill <me@ashbill.com>
2025-10-26 22:16:07 -0400

    🔧 同步KJS脚本（加入床、末地烛能坐）

--- pack/overrides/kubejs/server_scripts/fake_seats.js
+++ pack/overrides/kubejs/server_scripts/fake_seats.js
@@ -1,7 +1,10 @@
 ServerEvents.tags('block', event => {
     event.add('trainresync:fake_seats', [
+        'minecraft:white_bed',
+        'minecraft:red_bed',
+        'minecraft:end_rod',
+        'yuushya:toilet',
         'yuushya:couch_raw_crimson_wood',
-        'yuushua:toilet',
         'yuushya:park_bench_raw_oak_wood'
     ]);
     event.add('create:movable_empty_collider', [
