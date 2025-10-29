25aac581ee9605008c342e3bec080a2827528743
AshBill <me@ashbill.com>
2025-10-28 22:02:47 -0400

    :bulb: 清理KJS脚本（删除无用注释）

--- pack/overrides/kubejs/server_scripts/sprocket_from_diamond.js
+++ pack/overrides/kubejs/server_scripts/sprocket_from_diamond.js
@@ -1,11 +1,7 @@
-// kubejs/server_scripts/recipes_numismatics.js
-
 ServerEvents.recipes(event => {
-  // 1 diamond -> 1 sprocket
   event.shapeless('numismatics:sprocket', ['minecraft:diamond'])
        .id('kubejs:sprocket_from_diamond');
 
-  // 1 sprocket + 1 spur -> 1 diamond
   event.shapeless('minecraft:diamond', [
       'numismatics:sprocket',
       'numismatics:spur'
