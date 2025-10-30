99daa19dd0c67c2eea2945776a8447220c88c69d
AshBill <me@ashbill.com>
2025-10-30 00:22:54 -0400

    :pencil2: 修复「农夫绳网可用锦致麻绳合成」typo

--- pack/overrides/kubejs/server_scripts/safety_net_from_other_rope.js
+++ pack/overrides/kubejs/server_scripts/safety_net_from_other_rope.js
@@ -1,8 +1,12 @@
-ServerEvents.recipes(e => {
-  e.shaped('farmersdelight:safety_net', [
-    'RR',
-    'RR'
-  ], {
-    R: 'supplementaries:rope'
-  }).id('kubejs:safety_net_from_other_rope');
+ServerEvents.recipes(event => {
+    event.shaped(
+        Item.of('farmersdelight:safety_net', 1),
+        [
+            'RR',
+            'RR'
+        ],
+        {
+            R: 'supplementaries:rope'
+        }
+    ).id('kubejs:safety_net_from_other_rope');
 });
