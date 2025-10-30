329e73c7f7f089972faa704ce4d63a4afe6b2714
AshBill <me@ashbill.com>
2025-10-29 20:23:57 -0400

    🍻 兼容：农夫绳网可用锦致麻绳合成（KJS脚本）

---
+++ pack/overrides/kubejs/server_scripts/safety_net_from_other_rope.js
@@ -0,0 +1,8 @@
+ServerEvents.recipes(e => {
+  e.shaped('farmersdelight:safety_net', [
+    'RR',
+    'RR'
+  ], {
+    R: 'supplementaries:rope'
+  }).id('kubejs:safety_net_from_other_rope');
+});
